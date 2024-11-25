4f4ca19d335440caec8ba2f61a6d2674288c655d
Maxym <maxym0198@gmail.com>
2024-11-25 16:20:07 +0200

    add trainer hours service

---
+++ trainer-hours-service/src/test/java/com/gym/crm/microservices/trainer/hours/service/TrainerHoursServiceTests.java
@@ -0,0 +1,13 @@
+package com.gym.crm.microservices.trainer.hours.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerHoursServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
