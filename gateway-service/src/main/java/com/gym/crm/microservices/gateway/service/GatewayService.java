217cef4e7b8b3dc88034c0ac20d52b641351e842
Maxym <maxym0198@gmail.com>
2024-11-28 17:27:12 +0200

    GCA-22 implement gateway service

--- gateway-service/src/main/java/com/gym/crm/microservices/gateway/service/GatewayService.java
+++ gateway-service/src/main/java/com/gym/crm/microservices/gateway/service/GatewayService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class GatewayService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		SpringApplication.run(GatewayService.class, args);
 	}
 
 }
